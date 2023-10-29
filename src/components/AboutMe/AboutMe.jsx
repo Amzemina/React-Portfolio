a16cef54ef260b9745eef8321f11bc814e65816a
Ashley Zemina <amzemina@gmail.com>
2023-10-28 21:09:47 -0400

    added about me section, updated app.jsx and index

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <h1>About Me</h1>
+      <p>
+        I am a full stack web developer with a passion for learning and
+        creating. I am a graduate of the UCONN Coding Bootcamp. I am proficient 
+        in HTML, CSS, JavaScript, Node.js, Express.js, MySQL, MongoDB, and React.js. 
+        I am a team player and have experience working with a team of developers to create a full stack
+        application. I am a fast learner and I am always looking for ways to
+        improve my skills as a developer.
+      </p>
+    </div>
+  );
+}
+
+export default AboutMe;
