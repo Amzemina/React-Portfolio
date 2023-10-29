0a30949d988e27d12301d042b0649624aa03cc1f
Ashley Zemina <amzemina@gmail.com>
2023-10-28 21:37:01 -0400

    added contact form

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 // import Header from './components/Header/Header';
 import AboutMe from './components/AboutMe/AboutMe';
 // import Portfolio from './components/Portfolio/Portfolio';
-// import Contact from './components/Contact/Contact';
+import Contact from './components/Contact/Contact';
 // import Resume from './components/Resume/Resume';
 // import Footer from './components/Footer/Footer';
 
@@ -14,9 +14,9 @@ function App() {
       {/* <Header /> */}
       <Routes>
         <Route path="/" element={<AboutMe />} />
-        {/* <Route path="/portfolio" element={<Portfolio />} />
+        {/* <Route path="/portfolio" element={<Portfolio />} /> */}
         <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} /> */}
+        {/* <Route path="/resume" element={<Resume />} /> */}
       </Routes>
       {/* <Footer /> */}
     </div>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,58 @@
-// import  { Link } from 'react-router-dom';
-// import React from 'react';
+import { useState } from 'react';
 
+function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormState((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formState);
+
+    }
+    const { name, email, message } = formState;
+
+    return (
+        <section>
+            <h1>Contact me</h1>
+
+            <form onSubmit={handleSubmit}>
+                <div className="contact-form">
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={handleChange}
+                        name="name"
+                    />
+                </div>
+                <div className="contact-form">
+                    <input
+                        type="email"
+                        value={email}
+                        name="email"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="contact-form">
+                    <textarea
+                        name="message"
+                        value={message}
+                        onChange={handleChange}
+                        rows="5"
+                    />
+                </div>
+                <button className="submit-button" type="submit">
+                    Submit
+                </button>
+            </form>
+        </section>
+    );
+}
+
+export default Contact;
