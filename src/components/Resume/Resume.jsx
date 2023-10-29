edd4e326423b3319fbc778cd262855eca0ed7006
Ashley Zemina <amzemina@gmail.com>
2023-10-28 22:40:42 -0400

    added resume section, and downloadable resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,8 +1,39 @@
+import resumePDF from '../../assets/resume.pdf';
+
 function Resume() {
     return (
         <div>
-            <h1>Resume</h1>
-            <p></p>
+            <div>
+                <h2>Download my resume:</h2>
+                <a href={resumePDF} download>
+                    Download
+                </a>
+            </div>
+        
+            <div>
+                <h2>Front-end Proficiencies</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>JQuery</li>
+                    <li>responsive design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>   
+            </div>
+            <div>
+                <h2>Back-end Proficiencies</h2>
+                <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL, Sequelize</li>
+                    <li>MongoDB, Mongoose</li>
+                    <li>REST</li>
+                    <li>GraphQL</li>
+                </ul>
+            </div>
         </div>
     );
 }
