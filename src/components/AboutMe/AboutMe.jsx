b86bb102b7c29b321416ee243605c82595599a13
Ashley Zemina <amzemina@gmail.com>
2023-10-29 12:26:57 -0400

    updated styling

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -8,10 +8,9 @@ function AboutMe() {
 
       <p>
         I am a full stack web developer with a passion for learning and
-        creating. I am a graduate of the UCONN Coding Bootcamp. I am proficient 
-        in HTML, CSS, JavaScript, Node.js, Express.js, MySQL, MongoDB, and React.js. 
-        I am a team player and have experience working with a team of developers to create a full stack
-        application. I am a fast learner and I am always looking for ways to
+        creating. I am a graduate of the UCONN Coding Bootcamp where I learned to be proficient in MERN stack. 
+        I have experience working with various teams of developers to create multiple full stack
+        applications. I am a fast learner and I am always looking for ways to
         improve my skills as a developer.
       </p>
     </div>
