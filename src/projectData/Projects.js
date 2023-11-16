4602c9e1ac1f36934631fb2f846a898fabcabbc9
Ashley Zemina <amzemina@gmail.com>
2023-11-15 23:31:26 -0500

    added project

--- src/projectData/Projects.js
+++ src/projectData/Projects.js
@@ -3,7 +3,7 @@
 //project images
 import horiseonPNG from '../assets/horiseon.png';
 import fancyPNG from '../assets/Fancy.png';
-import rhythmPNG from '../assets/Rhythm.png';
+import loginPNG from '../assets/Login.png';
 import CodeQuizPNG from '../assets/CodeQuiz.png';
 import PasswordPNG from '../assets/Password.png';
 import WorkDayPNG from '../assets/WorkDay.png';
@@ -25,11 +25,11 @@ const projects = [
       github: 'https://github.com/ejoseph10/Fancy-Minute-Chef'
     },
     {
-      deployed: 'https://rhythm-reviews-8a43a75413f9.herokuapp.com/',
-      image: rhythmPNG,
+      deployed: 'https://rhythm-reviews-v2-a30efa7f58da.herokuapp.com/',
+      image: loginPNG,
       alt: 'screenshot of deployed Rhythm Reviews project',
       title: 'Rhythm-Reviews',
-      github: 'https://github.com/group7-project2/rhythm-reviews'
+      github: 'https://github.com/group2-project3/rhythm-reviews-2.0'
     },
     {
       deployed: 'https://amzemina.github.io/Code-Quiz/',
